dd19e03b205ef17cc878e5891a189a9f811af730
Mixaqi <mixaqimixaqi@mail.ru>
2024-02-23 12:03:49 +0300

    fixed navbrand

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
